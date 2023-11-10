53579a67bf754c241eb5c0741fdcea8a79b6dcaa
Minn <qaqmango@outlook.com>
2023-11-10 21:58:06 +0800

    update: 2023-11-10

---
+++ ReitoDMJ/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Get-ToolsLocation)\$env:ChocolateyPackageName"
+
+$fileLocation = 'tools\弹幕姬1.3.5.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  file          = $fileLocation
+  checksum      = 'E445B15BF88F8F2122765D9A230C451F9D86D3C1BBC5DA6D96C5B89E90C48ECE'
+  checksumType  = 'sha256'
+}
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "$DesktopPath\弹幕姬.lnk" -TargetPath "$toolsDir\弹幕姬\弹幕姬启动器.exe"
---
+++ ReitoDMJ/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Get-ToolsLocation)\$env:ChocolateyPackageName"
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Remove-Item -Recurse "$toolsDir"  -Force
+Remove-Item "$DesktopPath\弹幕姬.lnk"
