ecaa0eba5eea2db67e5c22d06fa28a2be2b7afee
Minn <qaqmango@outlook.com>
2023-11-16 16:21:56 +0800

    update: add olivevideoedit

---
+++ olive/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Get-ToolsLocation)\$env:ChocolateyPackageName"
+
+$fileLocation   = 'tools\Olive-55eedbfc-Windows-x86_64-Portable.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  file          = $fileLocation
+  checksum      = 'D73AEE3CB843B8B7CC9D9A234847FFD44B801BAAB1760E36062480364467D2FE'
+  checksumType  = 'sha256'
+}
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "$DesktopPath\Olive.lnk" -TargetPath "$toolsDir\olive-editor\olive-editor.exe"
