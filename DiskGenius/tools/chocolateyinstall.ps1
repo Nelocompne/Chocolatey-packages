53bb956d17a86a46a85c2c407792d35d57ea4a21
Minn <qaqmango@outlook.com>
2023-11-17 23:00:54 +0800

    update: add diskgenius

---
+++ DiskGenius/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Get-ToolsLocation)\$env:ChocolateyPackageName"
+
+$fileLocation   = 'tools\DG5511508_x86.zip'
+$fileLocation64 = 'tools\DG5511508_x64.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  file          = $fileLocation
+  file64        = $fileLocation64
+  checksum      = 'C49CCD0DB52E1B50557D384509E06C14DF4D42BA457C0642A590C8956408B9AD'
+  checksumType  = 'sha256'
+  checksum64    = '8C306AA776D8D13DCB264BCC304C5956BD199851387B282414B769B90BD456F1'
+  checksumType64= 'sha256'
+}
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "$DesktopPath\DiskGenius.lnk" -TargetPath "$toolsDir\DiskGenius\DiskGenius.exe"
