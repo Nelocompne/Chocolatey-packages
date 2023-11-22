c253fb7cb839e64eea76e8311de73e02dc173039
Minn <qaqmango@outlook.com>
2023-11-22 23:59:00 +0800

    update: diskinfo ver aoi v9.2.1

---
+++ CrystalDiskInfo-aoi/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Get-ToolsLocation)\$env:ChocolateyPackageName"
+
+$fileLocation   = 'tools\CrystalDiskInfo9_2_1Aoi.zip'
+$fileLocation64 = 'tools\CrystalDiskInfo9_2_1Aoi.zip'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  file          = $fileLocation
+  file64        = $fileLocation64
+  checksum      = 'E2B0DF505FC50B453C53E836753FBE390185BDB470AB339078302BFA3C1C2D9A'
+  checksumType  = 'sha256'
+  checksum64    = 'E2B0DF505FC50B453C53E836753FBE390185BDB470AB339078302BFA3C1C2D9A'
+  checksumType64= 'sha256'
+}
+
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+Install-ChocolateyZipPackage @packageArgs
+$osBitness = Get-ProcessorBits
+$ArtVersion = 'A'
+Install-ChocolateyShortcut -ShortcutFilePath "$DesktopPath\CrystalDiskInfo 9.lnk" -TargetPath "$toolsDir\DiskInfo$osBitness$ArtVersion.exe"
